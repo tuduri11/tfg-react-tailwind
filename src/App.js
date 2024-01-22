b54235287227042c8cef510f52b04ab7749f9de6
Antoni <atudurmo8@alumnes.ub.edu>
2024-01-22 20:00:30 +0100

    Primer commit con mi proyecto React y Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="bg-[rgb(155,23,24)]">
+          Edit <code className='text-'>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
