37d04f5f9f2cdddcc61e875a026cbbc7ae3a6458
Antoni <atudurmo8@alumnes.ub.edu>
2024-05-19 11:59:10 +0200

    Cambio servidor DNS

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const SERVER_DNS = "http://localhost:8000";
+export const SERVER_DNS = "http://44.220.151.18:8000";
 export const ACCESS_TOKEN_EXPIRE_TIME = 24*60*60*1000 //en milisegons
 export const URL_STRIPE_PREMIUM = "https://buy.stripe.com/test_6oE5nm0IEgkD9QkcMM"
